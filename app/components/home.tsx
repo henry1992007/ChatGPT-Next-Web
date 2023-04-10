2cc37aae9458dcd8a701f703d7f51cf54e166520
何建恒 <henry@hejianhengdeMacBook-Pro.local>
2023-04-10 15:00:17 +0800

    ad

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -119,10 +119,8 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
-          <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
-          </div>
+          <div className={styles["sidebar-title"]}>ChatGPT For Enterprise</div>
+          <div className={styles["sidebar-sub-title"]}></div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
           </div>
@@ -146,21 +144,21 @@ function _Home() {
                 onClick={chatStore.deleteSession}
               />
             </div>
-            <div className={styles["sidebar-action"]}>
-              <IconButton
-                icon={<SettingsIcon />}
-                onClick={() => {
-                  setOpenSettings(true);
-                  setShowSideBar(false);
-                }}
-                shadow
-              />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
-            </div>
+            {/*<div className={styles["sidebar-action"]}>*/}
+            {/*  <IconButton*/}
+            {/*    icon={<SettingsIcon />}*/}
+            {/*    onClick={() => {*/}
+            {/*      setOpenSettings(true);*/}
+            {/*      setShowSideBar(false);*/}
+            {/*    }}*/}
+            {/*    shadow*/}
+            {/*  />*/}
+            {/*</div>*/}
+            {/*<div className={styles["sidebar-action"]}>*/}
+            {/*  <a href={REPO_URL} target="_blank">*/}
+            {/*    <IconButton icon={<GithubIcon />} shadow />*/}
+            {/*  </a>*/}
+            {/*</div>*/}
           </div>
           <div>
             <IconButton
