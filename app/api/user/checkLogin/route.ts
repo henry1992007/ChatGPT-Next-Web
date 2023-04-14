e638a20007012ab51259cdfad18be27fa0a7dcaf
何建恒 <henry@hejianhengdeMacBook-Pro.local>
2023-04-14 23:50:52 +0800

    add

--- app/api/user/checkLogin/route.ts
+++ app/api/user/checkLogin/route.ts
@@ -1,18 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import fetch from "node-fetch";
 
-const redis = require("redis");
-
-const mysql = require("mysql2");
-
-const redisClient = redis.createClient({
-  url: "redis://127.0.0.1:6379",
-  //password: 'your-redis-password', // 如果有密码，请取消注释并填写
-});
-(async () => {
-  await redisClient.connect();
-})();
-
 export async function POST(req: NextRequest) {
   const reqData = await req.text();
   let response = await fetch("http://127.0.0.1:7817/user/login", {
--- app/api/user/createUser/route.ts
+++ app/api/user/createUser/route.ts
@@ -3,19 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { requestOpenai } from "../../common";
 import fetch, { RequestInfo } from "node-fetch";
 
-const redis = require("redis");
-// const mysql = require('mysql2');
-
-const c = (async function () {
-  const redisClient = redis.createClient({
-    url: "redis://127.0.0.1:6379",
-    //password: 'your-redis-password', // 如果有密码，请取消注释并填写
-  });
-
-  await redisClient.connect();
-  return redisClient;
-})();
-
 export async function POST(req: NextRequest) {
   console.log(req.body);
 
