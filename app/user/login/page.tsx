07c285868d0a6ede8efddf535ea27d1e14c47b23
何建恒 <henry@hejianhengdeMacBook-Pro.local>
2023-04-28 16:29:04 +0800

    Revert "Revert "add""
    This reverts commit 8417d9eb41c7ce0f02c0716e29e3a6e48c8a6366.

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -108,7 +108,8 @@ export default function Login() {
     return <Loading />;
   }
 
-  return resetPwd ? (
+  // return resetPwd ? (
+  return false ? (
     <div className={styles.loginContainer}>
       <h1 className={styles.title}>GTP ChatBot</h1>
       <input
